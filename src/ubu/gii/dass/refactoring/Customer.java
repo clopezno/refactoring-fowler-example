afba5318a01649ec29da4753e78c2cb39eb83689
Carlos <clopezno@ubu.es>
2014-03-18 11:10:26 +0100

    Refactoring code example

---
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -0,0 +1,79 @@
+package ubu.gii.dass.refactoring;
+
+/**
+* Tema  Refactorizaciones 
+*
+* Ejemplo de aplicación de refactorizaciones. Actualizado para colecciones genéricas de java 1.5
+*
+* @author M. Fowler y <A HREF="mailto:clopezno@ubu.es">Carlos López</A>
+* @version 1.1
+* @see java.io.File
+*
+*/
+import java.util.*;
+
+public class Customer {
+	private String _name;
+	private Vector<Rental> _rentals;
+
+	public Customer(String name) {
+		_name = name;
+		_rentals = new Vector<Rental>();
+
+	};
+
+	public void addRental(Rental arg) {
+		_rentals.addElement(arg);
+	}
+
+	public String getName() {
+		return _name;
+	};
+
+	public String statement() {
+		double totalAmount = 0;
+		int frequentRenterPoints = 0;
+		Enumeration<Rental> rentals = _rentals.elements();
+		String result = "Rental Record for " + getName() + "\n";
+		while (rentals.hasMoreElements()) {
+			double thisAmount = 0;
+			Rental each = rentals.nextElement();
+			thisAmount = amountFor(thisAmount, each);
+			// add frequent renter points
+			frequentRenterPoints++;
+			// add bonus for a two day new release rental
+			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+					&& each.getDaysRented() > 1)
+				frequentRenterPoints++;
+			// show figures for this rental
+			result += "\t" + each.getMovie().getTitle() + "\t"
+					+ String.valueOf(thisAmount) + "\n";
+			totalAmount += thisAmount;
+		}
+		// add footer lines
+		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+		result += "You earned " + String.valueOf(frequentRenterPoints)
+				+ " frequent renter points";
+		return result;
+	}
+
+	private double amountFor(double thisAmount, Rental each) {
+		// determine amounts for each line
+		switch (each.getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			thisAmount += 2;
+			if (each.getDaysRented() > 2)
+				thisAmount += (each.getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			thisAmount += each.getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			thisAmount += 1.5;
+			if (each.getDaysRented() > 3)
+				thisAmount += (each.getDaysRented() - 3) * 1.5;
+			break;
+		}
+		return thisAmount;
+	}
+}
