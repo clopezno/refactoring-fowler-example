1fca99ef6dda1edc238ca965f9831c643dca88e5
Carlos López <clopezno@ubu.es>
2019-04-05 13:29:01 +0200

    UTF8 Update Movie

--- src/ubu/gii/dass/refactoring/Movie.java
+++ src/ubu/gii/dass/refactoring/Movie.java
@@ -2,10 +2,10 @@ package ubu.gii.dass.refactoring;
 /**
  * Tema Refactorizaciones
  * 
- * Ejemplo de aplicación de refactorizaciones. Actualizado para colecciones
- * genéricas de java 1.5
+ * Ejemplo de aplicaciÃ³n de refactorizaciones. Actualizado para colecciones
+ * genÃ©ricas de java 1.5.
  * 
- * @author M. Fowler y <A HREF="mailto:clopezno@ubu.es">Carlos López</A>
+ * @author M. Fowler y <A HREF="mailto:clopezno@ubu.es">Carlos LÃ³pez</A>
  * @version 1.1
  * @see java.io.File
  * 
