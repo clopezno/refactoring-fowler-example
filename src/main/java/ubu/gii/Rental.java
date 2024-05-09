6e58d4532f54cf3bd89fd3445117ec5680593932
CristianFernandezM <cfm1001@alu.ubu.es>
2024-05-09 19:37:56 +0200

    #3 moveMethod frequentRenterPoints

--- src/main/java/ubu/gii/Rental.java
+++ src/main/java/ubu/gii/Rental.java
@@ -47,4 +47,13 @@ public class Rental {
 		return result;
 	}
 
+	int getFrequentRenterPoints(int frequentRenterPoints) {
+		frequentRenterPoints++;
+		// add bonus for a two day new release rental
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
+				&& getDaysRented() > 1)
+			frequentRenterPoints++;
+		return frequentRenterPoints;
+	}
+
 }
