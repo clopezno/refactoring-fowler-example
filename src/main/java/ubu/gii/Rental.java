017c2c4ec0b1b979fb19c465261af876b2fa9dc3
Saizcharly <83606181+saizchrly@users.noreply.github.com>
2024-05-09 19:23:30 +0200

    #3 Rename getCharge, change visibility getCharge

--- src/main/java/ubu/gii/Rental.java
+++ src/main/java/ubu/gii/Rental.java
@@ -27,4 +27,24 @@ public class Rental {
 		return _movie;
 	}
 
+	public double getCharge() {
+		double result = 0;
+		switch (getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			result += 2;
+			if (getDaysRented() > 2)
+				result += (getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			result += getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			result += 1.5;
+			if (getDaysRented() > 3)
+				result += (getDaysRented() - 3) * 1.5;
+			break;
+		}
+		return result;
+	}
+
 }
