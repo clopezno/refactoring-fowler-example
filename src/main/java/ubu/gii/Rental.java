219aa95a0cdbda14ccb0d16371185db97f3f8883
Alberto Lanchares Diez <118168519+Alanchares@users.noreply.github.com>
2024-05-14 15:00:08 +0200

    Add files via upload

---
+++ src/main/java/ubu/gii/Rental.java
@@ -0,0 +1,30 @@
+package ubu.gii;
+/**
+ * Tema Refactorizaciones
+ * 
+ * Ejemplo de aplicación de refactorizaciones. Actualizado para colecciones
+ * genéricas de java 1.5.
+ * 
+ * @author M. Fowler y <A HREF="mailto:clopezno@ubu.es">Carlos López</A>
+ * @version 1.1
+ * @see java.io.File
+ * 
+ */
+public class Rental {
+	private Movie _movie;
+	private int _daysRented;
+
+	public Rental(Movie movie, int daysRented) {
+		_movie = movie;
+		_daysRented = daysRented;
+	}
+
+	public int getDaysRented() {
+		return _daysRented;
+	}
+
+	public Movie getMovie() {
+		return _movie;
+	}
+
+}
