89d0ba9bcf4088735fb6c75a6b1ba8cafed67419
npi1001 <116637646+Nicop17@users.noreply.github.com>
2025-05-15 20:56:19 +0200

    Modificacion Price.java

--- src/ubu/gii/dass/refactoring/Price.java
+++ src/ubu/gii/dass/refactoring/Price.java
@@ -2,7 +2,7 @@ package ubu.gii.dass.refactoring;
 
 public abstract class Price {
     public abstract double getCharge(int daysRented);
-    public int getFrequentRenterPoints(int daysRented) {
+    public int getUpdatePoints(int daysRented) {
         return 1;
     }
 }
